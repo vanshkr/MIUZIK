c4cdadf6b862499879befb9369fddfa8204c5bd6
vanshkr <krvansh30@gmail.com>
2023-02-05 04:29:19 +0530

    .

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -16,7 +16,7 @@ const TopCharts = () => {
       <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">Discover Top Charts</h2>
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
-        {data?.filter(song=>song?.images?.coverart ).map((song, i) => (
+        {data?.filter(song=>song?.images?.coverart).map((song, i) => (
           <SongCard
             key={song.key}
             song={song}
--- src/redux/services/shazamCoreApi.js
+++ src/redux/services/shazamCoreApi.js
@@ -29,4 +29,6 @@ export const{
     useGetArtistDetailsQuery,
     useGetSongDetailsQuery,
     useGetSongRelatedQuery,
+    useGetSongsByGenreQuery,
+    useGetSongsByCountryQuery
 } = shazamCoreApi;
